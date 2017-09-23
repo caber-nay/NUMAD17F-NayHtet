21d76b87e928ba372f3f787739e78ef4f75b9fea
Nay Htet <htet.na@husky.neu.edu>
2017-09-23 12:33:18 -0700

    Dictionary Activity added (incomplete layout)

---
+++ app/src/main/java/edu/neu/madcourse/nayhtet/DictionaryActivity.java
@@ -0,0 +1,19 @@
+package edu.neu.madcourse.nayhtet;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * This Activity is called when Dictionary button is clicked
+ */
+
+public class DictionaryActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dictionary);
+        //Intent intent = getIntent();
+    }
+
+}
