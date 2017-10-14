f1dbb94cce7c539fd2b399e6ed77a447f7fe7686
Nay Htet <htet.na@husky.neu.edu>
2017-10-14 15:13:31 -0700

    Tile class added

--- app/src/main/java/edu/neu/madcourse/nayhtet/GameFragment.java
+++ app/src/main/java/edu/neu/madcourse/nayhtet/GameFragment.java
@@ -11,6 +11,18 @@ import android.view.ViewGroup;
  */
 public class GameFragment extends Fragment{
 
+    static private int[] mLargeIds = {R.id.large1,R.id.large2,R.id.large3,R.id.large4,
+            R.id.large5,R.id.large6,R.id.large7,R.id.large8,R.id.large9};
+    static private int[] mSmallIds = {R.id.small1,R.id.small2,R.id.small3,R.id.small4,
+            R.id.small5,R.id.small6,R.id.small7,R.id.small8,R.id.small9};
+
+    private Tile mGameBoard = new Tile(this);
+    private Tile[] mLargeTiles = new Tile[9];
+    private Tile[][] mSmallTiles = new Tile[9][9];
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true); // Retains this fragment instance across configuration changes
+    }
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_game,container,false);
--- app/src/main/java/edu/neu/madcourse/nayhtet/Tile.java
+++ app/src/main/java/edu/neu/madcourse/nayhtet/Tile.java
@@ -1,9 +1,34 @@
 package edu.neu.madcourse.nayhtet;
 
+import android.view.View;
+
 /**
  * Created by Nay Htet.
  */
 
 public class Tile {
 
+    private final GameFragment mGame;
+    private View mView;
+    private Character letter;
+    private Tile[] subTiles;
+
+    public Tile(GameFragment game) {
+        this.mGame = game;
+    }
+
+
+
+    public void setView(View view){
+        mView = view;
+    }
+    public View getView(){
+        return mView;
+    }
+    public void setSubTiles(Tile[] subTiles){
+        this.subTiles = subTiles;
+    }
+    public Tile[] getSubTiles(){
+        return subTiles;
+    }
 }
